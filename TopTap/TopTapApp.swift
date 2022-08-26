69c41d2e72ae295b919838b92e1e8a9e4e03339f
Anh-Toine <52642269+Anh-Toine@users.noreply.github.com>
2022-08-26 12:42:56 -0400

    Initial Commit

---
+++ TopTap/TopTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopTapApp.swift
+//  TopTap
+//
+//  Created by Lukas Nguyen on 2022-08-26.
+//
+
+import SwiftUI
+
+@main
+struct TopTapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
